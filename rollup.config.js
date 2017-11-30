5034909550b9d1959711f04fa8733a0b1a8acc0c
Kai <thiskaiguy@gmail.com>
2017-11-30 23:33:22 +0000

    Upgrade the rollup configuration
    Install babel-core (now required by rollup-plugin-babel)
    Remove the old build-module script
    Output 3 bundles from the configuration file for:
    - CommonJS
    - ES Modules
    - Universal Module Definition

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,34 @@
 import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
 
-export default {
-  entry: 'src/main.js',
-  format: 'umd',
-  moduleName: 'TiltEffect',
-  plugins: [ babel() ],
-  dest: 'dist/tilt-effect.js'
+const dev = process.env.NODE_ENV === 'development'
+
+const base = {
+  exports: 'default',
+  plugins: [babel()],
+  name: 'TiltEffect',
+  sourcemap: dev,
 }
+export default [
+  Object.assign({}, base, {
+    input: 'src/main-umd.js',
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+    },
+  }),
+  Object.assign({}, base, {
+    input: 'src/main-umd.js',
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+    },
+  }),
+  Object.assign({}, base, {
+    input: 'src/main-es.js',
+    output: {
+      file: pkg.module,
+      format: 'es',
+    },
+  }),
+]
